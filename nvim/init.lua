2008c8f803409dfe1312df792ee7b6a64ab27ce3
Sergei Kaznakhovskii <sergei.kaznakhovskii@prizepicks.com>
2026-05-27 13:46:44 -0600

    nvim: track telescope master for nvim 0.10+ LSP API fixes
    The 0.1.x branch hasn't been released since Jan 2024 and predates the
    position_encoding requirement on vim.lsp.util.make_position_params,
    which fires a deprecation warning on every gd/gr/gI. master has the fix
    and is what telescope maintainers effectively recommend now.

--- nvim/init.lua
+++ nvim/init.lua
@@ -238,7 +238,9 @@ require('lazy').setup({
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    -- Tracking master: 0.1.x is frozen and predates nvim 0.10's LSP
+    -- deprecations (e.g. make_position_params requires offset_encoding).
+    branch = 'master',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for install instructions
